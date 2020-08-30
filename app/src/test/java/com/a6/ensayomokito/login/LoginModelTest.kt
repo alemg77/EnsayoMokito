3b5c584a7054e6d58ac10c33d8aead6a5aa435f4
Alejandro <alemg77@yahoo.com.ar>
2020-08-30 12:43:24 -0300

    Segundo commit

--- app/src/test/java/com/a6/ensayomokito/login/LoginModelTest.kt
+++ app/src/test/java/com/a6/ensayomokito/login/LoginModelTest.kt
@@ -12,28 +12,29 @@ import org.mockito.junit.MockitoJUnitRunner
 class LoginModelTest {
 
     @Mock
-    private val presenter: Presenter? = null
-    private var model: LoginModel? = null
+    private lateinit var presenter: Presenter
+
+    private lateinit var model: LoginModel
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
-        model = LoginModel(presenter!!)
+        model = LoginModel(presenter)
     }
 
     @Test
     @Throws(Exception::class)
     fun shouldSuccerWithValidaUserPass() {
-        model!!.validaUser("ale", "123")
+        model.validaUser("ale", "123")
 
         // Verifica que se llama un metodo
-        Mockito.verify(presenter)?.usuarioValido()
+        Mockito.verify(presenter).usuarioValido()
     }
 
     @Test
     @Throws(Exception::class)
     fun shoulcNotSuccessWithThisUserandPass() {
-        model!!.validaUser("ale", "1234")
-        Mockito.verify(presenter)?.error()
+        model.validaUser("ale", "1234")
+        Mockito.verify(presenter).error()
     }
 }
